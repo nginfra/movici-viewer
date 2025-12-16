27aa2b5447bc3798ebc02a08efee3f8d6c61fb84
Pelle Koster <pellekoster@octagonal.nl>
2025-12-16 08:33:21 +0100

    add vue devtools to client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +17,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   build: {
     rollupOptions: {
       output: {
