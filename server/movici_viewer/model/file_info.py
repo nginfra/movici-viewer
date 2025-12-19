e6ef6f3a5638545b12377167fac9f2570ee3b455
Pelle Koster <pellekoster@octagonal.nl>
2025-12-19 10:37:48 +0100

    support non-entitybased datasets

--- server/movici_viewer/model/file_info.py
+++ server/movici_viewer/model/file_info.py
@@ -9,8 +9,28 @@ class DatasetFormat(enum.Enum):
     BINARY = "binary"
 
 
-@dataclasses.dataclass
+BINARY_DATASET_FILE_EXTENSIONS: dict[str | tuple[str, ...], str] = {
+    ".nc": "flooding_tape",
+    ".csv": "parameters",
+    ".tiff": "heightmap",
+    ".tif": "heightmap",
+    ".geotiff": "heightmap",
+}
+
+
+@dataclasses.dataclass(frozen=True)
 class InitDataInfo:
     name: str
-    path: Path
-    format: DatasetFormat
+    path: Path = dataclasses.field(compare=False)
+    format: DatasetFormat | None = dataclasses.field(compare=False, default=None)
+    type: str = dataclasses.field(compare=False, default="unknown")
+
+    @classmethod
+    def from_path(cls, path: Path):
+        return cls(path.stem, path)
+
+    def __post_init__(self):
+        if self.path.suffix in BINARY_DATASET_FILE_EXTENSIONS:
+            super().__setattr__("format", DatasetFormat.BINARY)
+            if self.type == "unknown":
+                super().__setattr__("type", BINARY_DATASET_FILE_EXTENSIONS[self.path.suffix])
